becfd67bc5da524f6c410d3e0bd3212e0724980a
Avabin <andrzej.piotrowski76@gmail.com>
2021-04-03 13:01:51 +0200

    Fix timezone

--- Source/Modules/Module.Core/Extensions/CronObservable.cs
+++ Source/Modules/Module.Core/Extensions/CronObservable.cs
@@ -8,13 +8,20 @@ namespace Module.Core.Extensions
     public static class CronObservable
     {
         public static string Jp2CronTab = "37 21 * * *";
-        public static string BlazeCronTab = "20 14 * * *";
+        public static string BlazeCronTab = "20 16 * * *";
         public static string Blaze2CronTab = "20 4 * * *";
+
+
+        static CronObservable()
+        {
+
+        }
         public static IObservable<int> Cron(string cron, IScheduler scheduler)
         {
+
             var schedule = CrontabSchedule.Parse(cron);
             return Observable.Generate(0, d => true, d => d + 1, d => d,
-                d => new DateTimeOffset(schedule.GetNextOccurrence(scheduler.Now.UtcDateTime.ToLocalTime())), scheduler);
+                d => scheduler.Now.ToLocalTime());
         }
     }
 }
